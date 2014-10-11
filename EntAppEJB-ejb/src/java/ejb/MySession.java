66c21713cb3837985b0fbc4ad4c7b7d01ca470cc
Thufir <hawat.thufir@gmail.com>
2014-10-11 13:32:22 -0700

    increment

--- EntAppEJB-ejb/src/java/ejb/MySession.java
+++ EntAppEJB-ejb/src/java/ejb/MySession.java
@@ -3,5 +3,10 @@ package ejb;
 import javax.ejb.Stateless;
 
 @Stateless
-public class MySession implements MySessionRemote {
+public class MySession implements EJBRemoteInterface {
+
+    @Override
+    public String getResult() {
+        return "This is My Session Bean";
+    }
 }
