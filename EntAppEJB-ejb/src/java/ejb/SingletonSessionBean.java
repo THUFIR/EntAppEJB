3df342330af143c5e424c7b5501be08f8ab6eaba
Thufir <hawat.thufir@gmail.com>
2014-10-16 00:19:54 -0700

    increment

--- EntAppEJB-ejb/src/java/ejb/SingletonSessionBean.java
+++ EntAppEJB-ejb/src/java/ejb/SingletonSessionBean.java
@@ -1,14 +1,18 @@
 package ejb;
 
+import java.util.logging.Logger;
 import javax.ejb.Singleton;
 
 @Singleton
 public class SingletonSessionBean implements SingletonSessionBeanRemoteInterface {
 
-    private int i = 0;
+    private static final Logger log = Logger.getLogger(SingletonSessionBean.class.getName());
+    private int i = 1003;
+    private ClientFacade cf;
 
     @Override
     public int next() {
+        log.info(cf.find(i).toString());
         return i++;
     }
 
